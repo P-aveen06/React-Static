e91b0131c7aab998f36c753cf9aea73d76f0af1d
P-aveen06 <paveenpradeep2@gamil.com>
2022-09-18 11:02:39 +0530

    first component

---
+++ index.js
@@ -0,0 +1,10 @@
+function FirstCmp()
+{
+    // In reat Class is termed as ClassName
+    return <p className="red">I am Paveen Kumar, This is my frist component</p>
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<FirstCmp />);
+const root1=ReactDOM.createRoot(document.getElementById('root1'));
+root1.render(<FirstCmp />);
