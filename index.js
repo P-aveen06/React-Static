d95033657520dc20f37089f28ae228d375a7127e
P-aveen06 <paveenpradeep2@gamil.com>
2022-09-18 14:14:58 +0530

    Somemore HTML code

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-function FirstCmp()
-{
-    // In reat Class is termed as ClassName
-    return <p className="red">I am Paveen Kumar, This is my frist component</p>
-}
+const Text=(props)=>{
+        /*we can directly write our HTML code in .js file this is done by the help of 
+        JSX -- Javascript Syntax Extension */
+        return <div>
+        <p className='black font-30px'>I am {props.fname} {props.lname}</p>
+        <lable>First Name:</lable><br/>
+        <input type='text' name='text' placeholder={props.fname} /><br/>
+        <lable>Last Name:</lable><br/>
+        <input type='text' name='text' placeholder={props.lname} />
+        </div>
+    }
+    let firstName=prompt("Enter your FirstName ?");
+    let lastName=prompt("Enter Your LastName ?");
+    const text=ReactDOM.createRoot(document.getElementById('root'));
+    text.render(<Text fname={firstName} lname={lastName}/>);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<FirstCmp />);
-const root1=ReactDOM.createRoot(document.getElementById('root1'));
-root1.render(<FirstCmp />);
