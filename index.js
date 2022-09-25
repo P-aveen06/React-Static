aa1f7ff39fa9ea0d250908801d215f225e1ea57d
P-aveen06 <paveenpradeep2@gamil.com>
2022-09-25 06:38:02 +0530

    added Date and Time

--- index.js
+++ index.js
@@ -25,11 +25,12 @@ const Greeting = () => {
 
 const AboutMe = () => {
   return (
-    <div className="text-justify" style={{marginLeft:"540px",marginRight:"100px"}}>
+    // style={{marginLeft:"540px",marginRight:"100px"}}
+    <div>
       <h1>About Me</h1>
       <p>
         Hii, Everyone My self Paveen Kumar,
-        <ul>
+        <ul style={{listStyle:"none"}}>
           <li>I'm Engineering Student</li>
           <li>20-year old Software Developer</li>
           <li>Crazy on Technology</li>
@@ -39,11 +40,13 @@ const AboutMe = () => {
   );
 };
 
-const Image=()=>{
-    return <div>
-        <img src="icon.png" alt="No Image" width="200px" height="200px"/>
+const Image = () => {
+  return (
+    <div>
+      <img src="icon.png" alt="No Image" width="200px" height="200px" />
     </div>
-}
+  );
+};
 
 const text = ReactDOM.createRoot(document.getElementById("root"));
 text.render(<Detail fname="Paveen" lname="Kumar" />);
@@ -51,8 +54,22 @@ text.render(<Detail fname="Paveen" lname="Kumar" />);
 const greeting = ReactDOM.createRoot(document.getElementById("mess"));
 greeting.render(<Greeting fname="Paveen" lname="Kumar" />);
 
-const aboutme=ReactDOM.createRoot(document.getElementById("aboutme"));
+const aboutme = ReactDOM.createRoot(document.getElementById("aboutme"));
 aboutme.render(<AboutMe />);
 
-const img=ReactDOM.createRoot(document.getElementById("img"));
+const img = ReactDOM.createRoot(document.getElementById("img"));
 img.render(<Image />);
+
+const root = ReactDOM.createRoot(document.getElementById('time'));
+  
+function tick() {
+const element = (
+    <div>
+    <h1>Hello, world!</h1>
+    <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+);
+root.render(element);
+}
+
+setInterval(tick, 1000);
